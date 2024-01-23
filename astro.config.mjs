a4bb58d5f09632b3baeba757c1a5e80e0403a6a2
Fabio Biondi <info@fabiobiondi.com>
2024-01-23 20:59:33 +0100

    add react tailwind mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import vercelServerless from '@astrojs/vercel/serverless';
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   adapter: vercelServerless(),
-
+  integrations: [react(), tailwind(), mdx()]
 });
