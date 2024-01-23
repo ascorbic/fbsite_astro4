a784dc95d51756951df75b6a1401f2608dd7ad47
Fabio Biondi <info@fabiobiondi.com>
2024-01-23 20:49:48 +0100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import vercelServerless from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'hybrid',
   adapter: vercelServerless(),
 
 });
