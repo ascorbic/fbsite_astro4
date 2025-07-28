1d0abca2d04e164a4276977dbd28f2157b047a76
Matt Kane <m@mk.gg>
2025-07-28 13:24:05 +0100

    Upgrade

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
-import vercelServerless from '@astrojs/vercel/serverless';
+import vercelServerless from '@astrojs/vercel';
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fbsite-astro4.vercel.app/',
-  output: 'hybrid',
+  site: 'https://fbsite-astro4-two.vercel.app/',
   adapter: vercelServerless(),
   /*
   vite: {
